6d42ed9d359820f84950a56e6ec4c5a3030b5c06
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-05-23 10:11:56 +0900

    useStateで完了TODOを表示

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./styles.css";
 
 export const App = () => {
   const [incompleteTodos, setIncompleteTodos] = useState(["aaa", "bbb", "aa"]);
+  const [completeTodos, setCompleteTodos] = useState(["ccc", "ddd"]);
   return (
     <>
       <div className="input-area">
@@ -26,14 +27,14 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <div className="list-row">
-            <li>完了TODO</li>
-            <button>戻す</button>
-          </div>
-          <div className="list-row">
-            <li>完了TODOその２</li>
-            <button>戻す</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
