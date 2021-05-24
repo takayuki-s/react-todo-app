b290204d7461bd0f0703865a6b9c104cb954d5b0
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-05-24 21:12:20 +0900

    TODOが5個以上の時はTODOを入力できない

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,7 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
       {incompleteTodos.length >= 5 && (
         <p style={{ color: "red" }}>
